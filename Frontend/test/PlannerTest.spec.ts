e42eff58cb818a87d2d84d90741307491f675f2d
vigeomart <veresmartin2006@gmail.com>
2026-03-27 13:28:43 +0100

    fix: remove setTimeout, because of timeout exceed

--- Frontend/test/PlannerTest.spec.ts
+++ Frontend/test/PlannerTest.spec.ts
@@ -1,52 +1,50 @@
-import {test,expect} from '@playwright/test'
+import {test,expect} from '@playwright/test';
 import { error } from 'console';
 
 test.describe('Leaflet and buttons test', () =>{
 
     test('check the start, end buttons are working',async ({page}) =>{
-            await page.goto('http://localhost:5173');
-            
-            await page.getByRole("link", {name: "Planner"}).click();
-            await expect(page).toHaveURL(/\/routes$/);
+        await page.goto('http://localhost:5173');
 
-            test.setTimeout(10_000);
+        await page.getByRole('link', {name: 'Planner'}).click();
+        await expect(page).toHaveURL(/\/routes$/);
 
-            const mapPointer = page.locator('.leaflet-container');
+        const mapPointer = page.locator('.leaflet-container');
 
-            expect(mapPointer).toBeVisible();
+        expect(mapPointer).toBeVisible();
 
-            const mapBound = await mapPointer.boundingBox();
-            if (!mapBound) {
-                throw new Error("The map is not visible.")
-            }
-            await page.getByRole("button", {name: "Set start"}).click();
+        const mapBound = await mapPointer.boundingBox();
+        if (!mapBound) {
+            throw new Error('The map is not visible.');
+        }
+        await page.getByRole('button', {name: 'Set start'}).click();
 
-            await page.mouse.click(mapBound.x + mapBound.width * 0.25, mapBound.y + mapBound.height*0.5 );
+        await page.mouse.click(mapBound.x + mapBound.width * 0.25, mapBound.y + mapBound.height * 0.5 );
 
-            await page.waitForTimeout(1000);
+        await page.waitForTimeout(1000);
 
-            await page.getByRole("button", {name: "Add Point"}).click();
+        await page.getByRole('button', {name: 'Add Point'}).click();
 
-            await page.mouse.click(mapBound.x + mapBound.width * 0.33, mapBound.y + mapBound.height*0.33 );
+        await page.mouse.click(mapBound.x + mapBound.width * 0.33, mapBound.y + mapBound.height * 0.33 );
 
-            await page.waitForTimeout(1000);
+        await page.waitForTimeout(1000);
 
-            await page.getByRole("button", {name: "Set end"}).click();
+        await page.getByRole('button', {name: 'Set end'}).click();
 
-            await page.mouse.click(mapBound.x + mapBound.width * 0.75, mapBound.y + mapBound.height*0.33 );
+        await page.mouse.click(mapBound.x + mapBound.width * 0.75, mapBound.y + mapBound.height * 0.33 );
 
-            await page.waitForTimeout(1000);
+        await page.waitForTimeout(1000);
 
-            var marker = page.locator('.leaflet-marker-icon');
+        let marker = page.locator('.leaflet-marker-icon');
 
-            expect(marker).toHaveCount(3);
+        expect(marker).toHaveCount(3);
 
-            await page.getByRole("button", {name: "Clear route"}).click();
+        await page.getByRole('button', {name: 'Clear route'}).click();
 
-            await page.waitForTimeout(1000);
+        await page.waitForTimeout(1000);
 
-            marker = page.locator('.leaflet-marker-icon');
-            
-            expect(marker).toHaveCount(0);
+        marker = page.locator('.leaflet-marker-icon');
+
+        expect(marker).toHaveCount(0);
     });
 });
