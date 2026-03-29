ff203b6e6789ac0e2555173da23ce8744bda1dd7
István <histvan1510@gmail.com>
2026-03-29 19:06:20 +0200

    add user registration endpoint, password hashing and DTO validation

---
+++ Backend/evoHike.Backend/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using evoHike.Backend.Data;
+using evoHike.Backend.Models;
+using evoHike.Backend.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace evoHike.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class  AuthController : ControllerBase
+    {
+        private readonly EvoHikeContext _context;
+        public AuthController(EvoHikeContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserRegistrationDTO request)
+        {
+            bool emailExist = await _context.Users.AnyAsync(u => u.Email == request.Email);
+            if (emailExist)
+            {
+                return BadRequest("This email is already registered.");
+            }
+
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            var newUser = new User
+            {
+                Username = request.Username,
+                Email = request.Email,
+                PasswordHash = passwordHash
+            };
+
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+
+
+            return Ok("User registered successfully.");
+        }
+    }
+}
---
+++ Backend/evoHike.Backend/Models/DTO/UserRegistrationDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace evoHike.Backend.Models.DTO
+{
+    public class UserRegistrationDTO
+    {
+        [Required (ErrorMessage = "Username is required!")]
+        [Length(3, 20, ErrorMessage = "Username must be at least 3 characters long!")]
+        public string Username { get; set; } = null!;
+
+        [Required (ErrorMessage = "Email is required!")]
+        [EmailAddress (ErrorMessage = "A valid email address is required!")]
+        public string Email { get; set; } = null!;
+
+        [Required (ErrorMessage = "Password is required!")]
+        [Length(6,100, ErrorMessage = "The password must be at least 6 character long.")]
+        public string Password { get; set; } = null!;
+    }
+}
