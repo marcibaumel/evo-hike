3fb7533583054c2775b43c54fa8457125ab6536e
István <histvan1510@gmail.com>
2026-04-07 15:43:34 +0200

    -Generated JWT token -Secret key is hidden

--- Backend/evoHike.Backend/Controllers/AuthController.cs
+++ Backend/evoHike.Backend/Controllers/AuthController.cs
@@ -3,6 +3,10 @@ using evoHike.Backend.Models;
 using evoHike.Backend.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
 namespace evoHike.Backend.Controllers
 {
@@ -11,9 +15,11 @@ namespace evoHike.Backend.Controllers
     public class  AuthController : ControllerBase
     {
         private readonly EvoHikeContext _context;
-        public AuthController(EvoHikeContext context)
+        private readonly IConfiguration _configuration;
+        public AuthController(EvoHikeContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         [HttpPost("register")]
@@ -51,7 +57,34 @@ namespace evoHike.Backend.Controllers
             {
                 return Unauthorized("Invalid password.");
             }
-            return Ok("Login successful.");
+            string token = CreateToken(user);
+            return Ok(new { message = "Login successful.", token });
+        }
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var secretKey = _configuration.GetSection("Jwt:Key").Value;
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
