89bda6e150e198ad4d85ad98a8bdd979b570a5a8
István <histvan1510@gmail.com>
2026-04-22 14:00:47 +0200

    added protected routes and authcontext

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -5,14 +5,17 @@ import App from './App.tsx';
 import './i18n/index.ts';
 import Navbar from './pages/Layout/Navbar.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
-            <Navbar />
-            <main className="min-h-screen">
-                <App />
-            </main>
-        </BrowserRouter>
+        <AuthProvider>
+            <BrowserRouter>
+                <Navbar />
+                <main className="min-h-screen">
+                    <App />
+                </main>
+            </BrowserRouter>
+        </AuthProvider>
     </StrictMode>
 );
