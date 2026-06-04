16aa5dbd5fedc354280f1d81b408503a2149b81a
Pejkó Bálint <61847355+balintpejko@users.noreply.github.com>
2026-06-04 20:32:12 +0200

    added translation

--- Frontend/src/utils/difficulty.ts
+++ Frontend/src/utils/difficulty.ts
@@ -1,11 +1,14 @@
+import { useTranslation } from "react-i18next";
+
 export type DifficultyLevel = 0 | 1 | 2 | 3;
 
 export const getDifficultyLabel = (level: DifficultyLevel): string => {
+    const {t} = useTranslation();
     switch (level) {
-    case 0: return 'Easy';
-    case 1: return 'Moderate';
-    case 2: return 'Hard';
-    case 3: return 'Expert';
+    case 0: return t("trail.difficulties.0");
+    case 1: return  t("trail.difficulties.1");
+    case 2: return  t("trail.difficulties.2");
+    case 3: return  t("trail.difficulties.3");
     default: return '';
     }
 };
