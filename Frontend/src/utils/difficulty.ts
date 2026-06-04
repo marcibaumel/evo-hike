8e418aac4a04a500fd91df899f5e9e396907dac1
Pejkó Bálint <61847355+balintpejko@users.noreply.github.com>
2026-06-04 20:33:50 +0200

    eslint fix

--- Frontend/src/utils/difficulty.ts
+++ Frontend/src/utils/difficulty.ts
@@ -1,14 +1,14 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
 export type DifficultyLevel = 0 | 1 | 2 | 3;
 
 export const getDifficultyLabel = (level: DifficultyLevel): string => {
     const {t} = useTranslation();
     switch (level) {
-    case 0: return t("trail.difficulties.0");
-    case 1: return  t("trail.difficulties.1");
-    case 2: return  t("trail.difficulties.2");
-    case 3: return  t("trail.difficulties.3");
+    case 0: return t('trail.difficulties.0');
+    case 1: return  t('trail.difficulties.1');
+    case 2: return  t('trail.difficulties.2');
+    case 3: return  t('trail.difficulties.3');
     default: return '';
     }
 };
