c74be7a2a132ae78b39e57e84a9fcb75116c5c37
PolonkaiGabor <gaborpolonkai6@gmail.com>
2026-07-06 00:07:00 +0200

    migrations

---
+++ Backend/evoHike.Backend/Migrations/20260705212823_AddControlPointsToTrail.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+
+#nullable disable
+
+namespace evoHike.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddControlPointsToTrail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Point>(
+                name: "EndPoint",
+                table: "HikingTrails",
+                type: "geography",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Point>(
+                name: "StartPoint",
+                table: "HikingTrails",
+                type: "geography",
+                nullable: true);
+
+            migrationBuilder.AddColumn<MultiPoint>(
+                name: "Waypoints",
+                table: "HikingTrails",
+                type: "geography",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndPoint",
+                table: "HikingTrails");
+
+            migrationBuilder.DropColumn(
+                name: "StartPoint",
+                table: "HikingTrails");
+
+            migrationBuilder.DropColumn(
+                name: "Waypoints",
+                table: "HikingTrails");
+        }
+    }
+}
